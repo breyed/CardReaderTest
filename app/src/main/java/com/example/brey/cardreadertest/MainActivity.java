17dd82ff029b1f3ed229986e85fae29f86840d13
Edward Brey <edward.brey@toolsay.com>
2017-03-07 19:03:07 -0600

    Pull out handler into its own class to make minimal test more like SDK demo.

--- app/src/main/java/com/example/brey/cardreadertest/MainActivity.java
+++ app/src/main/java/com/example/brey/cardreadertest/MainActivity.java
@@ -15,7 +15,10 @@ import com.magtek.mobile.android.mtlib.MTSCRAEvent;
 
 public class MainActivity extends AppCompatActivity {
 
+    private AudioManager audioManager;
+    private int initialVolume, maxVolume;
     private MTSCRA cardReader;
+    private Handler cardReaderHandler = new CardReaderHandler();
     private TextView hello;
 
     @Override
@@ -24,31 +27,39 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         hello = (TextView) findViewById(R.id.hello);
 
-        final AudioManager audioManager = (AudioManager) getSystemService(Context.AUDIO_SERVICE);
-        final int initialVolume = audioManager.getStreamVolume(AudioManager.STREAM_MUSIC);
-        final int maxVolume = audioManager.getStreamMaxVolume(AudioManager.STREAM_MUSIC);
+        audioManager = (AudioManager) getSystemService(Context.AUDIO_SERVICE);
+        initialVolume = audioManager.getStreamVolume(AudioManager.STREAM_MUSIC);
+        maxVolume = audioManager.getStreamMaxVolume(AudioManager.STREAM_MUSIC);
         audioManager.setStreamVolume(AudioManager.STREAM_MUSIC, maxVolume, AudioManager.FLAG_SHOW_UI);
-        cardReader = new MTSCRA(this, new Handler() {
-            @Override public void handleMessage(Message msg) {
-                hello.setText("Message");
-                switch (msg.what) {
-                    case MTSCRAEvent.OnDataReceived:
-                        hello.setText("Data received");
-                        if (cardReader.getTrack1().length() == 0 || cardReader.getTrack2().length() == 0 || cardReader.getKSN().length() == 0 || cardReader.getMaskedTracks().length() == 0) return; // Ignores partial swipes.
-                        hello.setText(cardReader.getMaskedTracks());
-                        break;
-
-                    case MTSCRAEvent.OnDeviceConnectionStateChanged:
-                        hello.setText("Change");
-                        switch ((MTConnectionState) msg.obj) {
-                            case Disconnected: audioManager.setStreamVolume(AudioManager.STREAM_MUSIC, initialVolume, AudioManager.FLAG_SHOW_UI); break;
-                            case Connected: audioManager.setStreamVolume(AudioManager.STREAM_MUSIC, maxVolume, AudioManager.FLAG_SHOW_UI); break;
-                        }
-                }
-            }
-        });
+        cardReader = new MTSCRA(this, cardReaderHandler);
         cardReader.setConnectionType(MTConnectionType.Audio);
         cardReader.openDevice();
         hello.setText("Opened");
+    }
+
+    private class CardReaderHandler extends Handler {
+        @Override
+        public void handleMessage(Message msg) {
+            hello.setText("Message");
+            switch (msg.what) {
+                case MTSCRAEvent.OnDataReceived:
+                    hello.setText("Data received");
+                    if (cardReader.getTrack1().length() == 0 || cardReader.getTrack2().length() == 0 || cardReader.getKSN().length() == 0 || cardReader.getMaskedTracks().length() == 0)
+                        return; // Ignores partial swipes.
+                    hello.setText(cardReader.getMaskedTracks());
+                    break;
+
+                case MTSCRAEvent.OnDeviceConnectionStateChanged:
+                    hello.setText("Change");
+                    switch ((MTConnectionState) msg.obj) {
+                        case Disconnected:
+                            audioManager.setStreamVolume(AudioManager.STREAM_MUSIC, initialVolume, AudioManager.FLAG_SHOW_UI);
+                            break;
+                        case Connected:
+                            audioManager.setStreamVolume(AudioManager.STREAM_MUSIC, maxVolume, AudioManager.FLAG_SHOW_UI);
+                            break;
+                    }
+            }
+        }
     }
 }
